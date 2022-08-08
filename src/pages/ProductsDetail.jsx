7901f03e58ffbfa10aa8f2fb76624c0697f53d71
Ulises Urbina <ulisesurbinam@gmail.com>
2022-08-08 14:48:48 -0500

    ecommerce

--- src/pages/ProductsDetail.jsx
+++ src/pages/ProductsDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Carousel, Row } from 'react-bootstrap';
+import { Carousel, Col, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux/es/exports';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getProductsThunk } from '../store/slices/products.slice';
@@ -30,7 +30,7 @@ const ProductsDetail = () => {
 
     console.log(suggestedProducts)
 
-    const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(1);
     const increment = () => {
         setCounter(counter + 1)
     }
@@ -40,44 +40,46 @@ const ProductsDetail = () => {
 
     return (
         <div>
-            {/* <Row xs={1} md={2} lg={2}> */}
-                <div className="DetailContainer">
-                    <div className="ImgDetail">
-                        <img src={productDet?.productImgs} alt="imageProduct" />
-                    </div>
-                    <div className="InfoDetail">
-                        <h1>{productDet?.title}</h1>
-                        <h4>Category: {productDet?.category?.name}</h4>
-                        <hr />
-                        <h3><b>Price: $ {productDet?.price}</b></h3>
-                        <div className="Counter">
-                            <h3><b>Quality:</b> {counter} Pza(s)</h3>
-                            <button onClick={increment}><i className="fa-solid fa-angle-up"></i></button>
-                            <button onClick={decrement}><i className="fa-solid fa-angle-down"></i></button>
-                        </div>
-                        <hr />
-                        <h6>{productDet?.description}</h6>
-                    </div>
+            <div className="DetailContainer">
+                <div className="ImgDetail">
+                    <img src={productDet?.productImgs} alt="imageProduct" />
                 </div>
-            {/* </Row> */}
-            <div className="SuggestedProducts">
-                <h3>Suggested Products</h3>
-                <Carousel variant="dark">
-                    {
-                        suggestedProducts.map((suggested) => (
-                            <Carousel.Item>
-                                <div className="ImgSuggested" key={suggested.id}>
-                                    <img className="d-block w-100" alt="First slide" onClick={() => navigate(`/products/${suggested.id}`)} src={suggested.productImgs} />
-                                </div>
-                                <div style={{ textAlign: "center" }}>
-                                    <h4 onClick={() => navigate(`/products/${suggested.id}`)}>
-                                        {suggested.title}
-                                    </h4>
-                                </div>
-                            </Carousel.Item>
-                        ))
-                    }
-                </Carousel>
+                <div className="InfoDetail">
+                    <h1>{productDet?.title}</h1>
+                    <h4>Category: {productDet?.category?.name}</h4>
+                    <hr />
+                    <h3><b>Price: $ {productDet?.price}</b></h3>
+                    <div className="Counter">
+                        <h3><b>Quality:</b> {counter} Pza(s)</h3>
+                        <button onClick={increment}><i className="fa-solid fa-angle-up"></i></button>
+                        <button onClick={decrement}><i className="fa-solid fa-angle-down"></i></button>
+                    </div>
+                    <hr />
+                    <h6>{productDet?.description}</h6>
+                </div>
+            </div>
+            <div className="SuggestedProducts container">
+                <div style={{ textAlign: "center" }}>
+                    <h3><b>Suggested Products</b></h3>
+                </div>
+                <div>
+                    <Carousel variant="dark">
+                        {
+                            suggestedProducts.map((suggested) => (
+                                <Carousel.Item>
+                                    <div className="ImgSuggested" key={suggested.id}>
+                                        <img className="d-block w-100" alt="First slide" onClick={() => navigate(`/products/${suggested.id}`)} src={suggested.productImgs} />
+                                    </div>
+                                    <div style={{ textAlign: "center" }}>
+                                        <h5 onClick={() => navigate(`/products/${suggested.id}`)}>
+                                            {suggested.title}
+                                        </h5>
+                                    </div>
+                                </Carousel.Item>
+                            ))
+                        }
+                    </Carousel>
+                </div>
             </div>
         </div>
     );
